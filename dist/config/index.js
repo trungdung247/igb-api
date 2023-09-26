6513377b08d6eefa2b0ac845a7a08f7b72bc6a3d
Phạm Dũng <trungdung24794@gmail.com>
2023-09-26 10:36:37 +0700

    add babel build option

---
+++ dist/config/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  urlDB: process.env.DB_URL,
+  port: process.env.SERVER_PORT,
+  local_client_app: process.env.LOCAL_CLIENT_APP,
+  remote_client_app: process.env.REMOTE_CLIENT_APP,
+  allowedDomains: process.env.NODE_ENV === 'production' ? [process.env.REMOTE_CLIENT_APP, process.env.REMOTE_SERVER_API] : [process.env.LOCAL_CLIENT_APP, process.env.LOCAL_SERVER_API],
+  pathImage: process.env.NODE_ENV === 'production' ? process.env.REMOTE_SERVER_API : process.env.LOCAL_SERVER_API,
+  listLanguages: ["vi", "en"]
+};
---
+++ dist/index.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const routes = require('./src/routes/routes');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const cors = require("cors");
+const path = require('path');
+const app = express();
+const config = require('./config');
+const {
+  port,
+  allowedDomains
+} = config;
+const PORT = port || 8797;
+const db = mongoose.connection;
+
+//connect db
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: false,
+  useUnifiedTopology: true
+}).then(() => console.log('DB Connected!'));
+db.on('error', err => {
+  console.log('DB connection error:', err.message);
+});
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
+app.use(bodyParser.json({
+  limit: '50mb'
+}));
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true
+}));
+app.use(expressValidator());
+app.use('/api', routes);
+app.listen(PORT, () => {
+  console.log("Server API started");
+});
+module.exports = app;
